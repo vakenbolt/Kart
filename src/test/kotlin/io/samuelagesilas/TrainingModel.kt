f533088cde00753694c5ba5fe71b495f7edd3acd
Samuel A <desk.samuel.a@gmail.com>
2019-04-01 20:42:56 -0500

    Added more tests.

--- src/test/kotlin/io/samuelagesilas/TrainingModel.kt
+++ src/test/kotlin/io/samuelagesilas/TrainingModel.kt
@@ -35,29 +35,39 @@ object QuestionLabels {
     const val Q5 = "Question 5"
 }
 
-val trainingModel: List<DecisionTreeClassifierDataRow<Diagnosis>> = listOf(DataRow(
-    Symptom1,
-    Symptom2,
-    DiagnosisA
-),
-                                                                                                               DataRow(
-                                                                                                                   Symptom1,
-                                                                                                                   Symptom3,
-                                                                                                                   DiagnosisB
-                                                                                                               ),
-                                                                                                               DataRow(
-                                                                                                                   Symptom4,
-                                                                                                                   Symptom5,
-                                                                                                                   DiagnosisC
-                                                                                                               ),
-                                                                                                               DataRow(
-                                                                                                                   Symptom1,
-                                                                                                                   Symptom3,
-                                                                                                                   DiagnosisD
-                                                                                                               ),
-                                                                                                               DataRow(
-                                                                                                                   Symptom1,
-                                                                                                                   Symptom5,
-                                                                                                                   DiagnosisE
-                                                                                                               )
+val trainingModel: List<DecisionTreeClassifierDataRow<Diagnosis>> = listOf(DataRow(Symptom1, Symptom2, DiagnosisA),
+                                                                           DataRow(Symptom1, Symptom1, DiagnosisB),
+                                                                           DataRow(Symptom1, Symptom5, DiagnosisC),
+                                                                           DataRow(Symptom1, Symptom3, DiagnosisD),
+                                                                           DataRow(Symptom5, Symptom3, DiagnosisE),
+                                                                           DataRow(Symptom1, Symptom4, DiagnosisB),
+                                                                           DataRow(Symptom1, Symptom1, DiagnosisC),
+                                                                           DataRow(Symptom2, Symptom3, DiagnosisC)
 )
+
+object Questions {
+    val q1: PredicateFunction<DataRow<Diagnosis>> = PredicateFunction(label = QuestionLabels.Q1) {
+        it.diagnosisSymptom1 == Symptom1 || it.diagnosisSymptom2 == Symptom5
+    }
+    val q2: PredicateFunction<DataRow<Diagnosis>> = PredicateFunction(label = QuestionLabels.Q2) {
+        it.diagnosisSymptom1 == Symptom1 || it.diagnosisSymptom2 == Symptom3
+    }
+    val q3: PredicateFunction<DataRow<Diagnosis>> = PredicateFunction(label = QuestionLabels.Q3) {
+        it.diagnosisSymptom1 == Symptom3 || it.diagnosisSymptom2 == Symptom3
+    }
+    val q4: PredicateFunction<DataRow<Diagnosis>> = PredicateFunction(label = QuestionLabels.Q4) {
+        it.diagnosisSymptom1 == Symptom4 || it.diagnosisSymptom2 == Symptom4
+    }
+    val q5: PredicateFunction<DataRow<Diagnosis>> = PredicateFunction(label = QuestionLabels.Q5) {
+        it.diagnosisSymptom1 == Symptom5 || it.diagnosisSymptom2 == Symptom5
+    }
+
+    val predicates: List<PredicateFunction<DataRow<Diagnosis>>> = listOf(Questions.q1,
+                                                                         Questions.q2,
+                                                                         Questions.q3,
+                                                                         Questions.q4,
+                                                                         Questions.q5)
+}
+
+
+val classifier: DecisionTreeClassifier<Diagnosis> = DecisionTreeClassifier(trainingModel, Questions.predicates)
